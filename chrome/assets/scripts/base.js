c05ab4ff4b035323b2128c1a7dcdce089e6542d2
Greg Cooper <greg@stripe.com>
2015-04-22 09:04:38 -0700

    Updated javascript file

--- chrome/assets/scripts/base.js
+++ chrome/assets/scripts/base.js
@@ -1,27 +1,35 @@
 $(function(){
 
-  // header
-  $('a#shuf').insertAfter($('div#header ul.menu'));
-  $('div#filter').insertBefore($('form#search-form'));
+  var do_stuff = function() {
 
-  // track
-  $('div.section-player').each(function () {
+    // header
+    $('a#shuf').insertAfter($('div#header ul.menu'));
+    $('div#filter').insertBefore($('form#search-form'));
 
-    // create the controls element
-    $(this).find('span.share-links, div.meta, ul.tools').addClass('control-latch');
-    $(this).find('.control-latch').wrapAll('<div class="controls">');
+    // track
+    $('div.section-player').each(function () {
 
-    // tidies up the sharing elements
-    $(this).find('span.share-links').each(function () {
+      // create the controls element
+      $(this).find('span.share-links, div.meta, ul.tools').addClass('control-latch');
+      $(this).find('> .control-latch').wrapAll('<div class="controls">');
 
-      $(this).find('.twitter-share, .facebook-share').wrapAll('<ul class="sharemenu">');
+      // tidies up the sharing elements
+      $(this).find('span.share-links').each(function () {
+
+        $(this).find('.twitter-share, .facebook-share').wrapAll('<ul class="sharemenu">');
+
+      });
+
+      // moves play/pause control
+      $(this).find('li.playdiv').detach().appendTo(this);
+      $(this).find('li.playdiv').wrapAll('<ul class="playpause">');
 
     });
 
-    // moves play/pause control
-    $(this).find('li.playdiv').detach().appendTo(this);
-    $(this).find('li.playdiv').wrapAll('<ul class="playpause">');
+  };
 
-  });
+  do_stuff();
+
+  $(document).on('ajaxComplete', do_stuff());
 
 });
