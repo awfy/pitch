7a554822ec6ca7c161db905a04b7ddf0238ffe84
Greg Cooper <greg@stripe.com>
2015-04-21 19:45:30 -0700

    Updated track player

--- chrome/assets/scripts/base.js
+++ chrome/assets/scripts/base.js
@@ -1,13 +1,27 @@
-// Moving elements to/around the header
-$('#shuf').insertAfter($('#header ul.menu'));
-$('#filter').insertBefore($('#search-form'));
+$(function(){
 
+  // header
+  $('a#shuf').insertAfter($('div#header ul.menu'));
+  $('div#filter').insertBefore($('form#search-form'));
 
-// Runs when site is finished loading
-$( window ).load(function() {
+  // track
+  $('div.section-player').each(function () {
 
-  // Remove bs from the welcome message
-  $('#message-intro-new').find('br').remove();
+    // create the controls element
+    $(this).find('span.share-links, div.meta, ul.tools').addClass('control-latch');
+    $(this).find('.control-latch').wrapAll('<div class="controls">');
+
+    // tidies up the sharing elements
+    $(this).find('span.share-links').each(function () {
+
+      $(this).find('.twitter-share, .facebook-share').wrapAll('<ul class="sharemenu">');
+
+    });
+
+    // moves play/pause control
+    $(this).find('li.playdiv').detach().appendTo(this);
+    $(this).find('li.playdiv').wrapAll('<ul class="playpause">');
+
+  });
 
 });
-
