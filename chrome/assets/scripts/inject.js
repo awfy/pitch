dff347118344098432b98ca29040cf97f22de026
Greg Cooper <greg@stripe.com>
2015-04-26 12:03:40 -0700

    Fixed a JS error caused by a uncompiled inject file

--- chrome/assets/scripts/inject.js
+++ chrome/assets/scripts/inject.js
@@ -1,13 +1,5 @@
 $(function(){
   var s = document.createElement('script');
-  s.src = chrome.extension.getURL('assets/scripts/jquery-2.1.3.min.js');
-  (document.head||document.documentElement).appendChild(s);
-  s.onload = function() {
-    s.parentNode.removeChild(s);
-  };
-});
-$(function(){
-  var s = document.createElement('script');
   s.src = chrome.extension.getURL('assets/scripts/base.min.js');
   (document.head||document.documentElement).appendChild(s);
   s.onload = function() {
